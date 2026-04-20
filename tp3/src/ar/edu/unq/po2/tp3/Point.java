5e7ff00d0842fc910973dca4e0ea2fbffde12e59
Juan Manuel Lopetegui <lopeteguijuanma@gmail.com>
2026-04-20 10:11:52 -0300

    falta 9 y 10

--- tp3/src/ar/edu/unq/po2/tp3/Point.java
+++ tp3/src/ar/edu/unq/po2/tp3/Point.java
@@ -14,6 +14,14 @@ public class Point {
 		this.y = y;
 	}
 	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
 	public void moveTo(int x, int y) {
 		this.x = x;
 		this.y = y; 
---
+++ tp3/src/ar/edu/unq/po2/tp3/Rectangulo.java
@@ -0,0 +1,10 @@
+package ar.edu.unq.po2.tp3;
+
+public class Rectangulo {
+	
+	private Point origen;
+	private int ancho;
+	private int alto;
+
+}
+ 
