8aa7f72d8322e89f925c39eab37ab0da7d9fb5ed
Juan Manuel Lopetegui <lopeteguijuanma@gmail.com>
2026-04-26 08:35:07 -0300

    Rectángulo terminado

--- tp3/src/ar/edu/unq/po2/tp3/Rectangulo.java
+++ tp3/src/ar/edu/unq/po2/tp3/Rectangulo.java
@@ -5,6 +5,14 @@ public class Rectangulo {
 	private Point origen;
 	private int ancho;
 	private int alto;
-
+	
+	public Rectangulo(Point origen, int ancho, int alto) {
+		
+		// Precondición: ancho > 0 && alto >0
+		
+		this.origen = origen;
+		this.ancho = ancho;
+		this.alto = alto; 		
+	}
 }
- 
+
