5b990cd43d4adecbc763114986a0dcc6be0af56b
Juan Manuel Lopetegui <lopeteguijuanma@gmail.com>
2026-04-26 08:48:56 -0300

    Cuadrado hecho

---
+++ tp3/src/ar/edu/unq/po2/tp3/Cuadrado.java
@@ -0,0 +1,9 @@
+package ar.edu.unq.po2.tp3;
+
+public class Cuadrado extends Rectangulo {
+	public Cuadrado(Point origen, int lado) {
+		// Precondición: lado > 0
+		super(origen, lado, lado);
+	}
+
+}
